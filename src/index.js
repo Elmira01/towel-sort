f142cee86566219aa6d3ba66a740f9e8887d15d1
Elmira Grigoryan <106161682+Elmira01@users.noreply.github.com>
2022-08-12 17:01:26 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 
 // You should implement your task here.
+const arr = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
+function towelSort(){
+    console.log(
+  arr.reduce((acc, cur, i) => {
+    cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
+    return acc;
+  }, [])
+);
+}
+
+
 
 module.exports = function towelSort (matrix) {
   return [];
